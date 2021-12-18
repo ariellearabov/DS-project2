a3a0974c8afbad6bd1eb034be1f18a630d08a850
Yuval Abeles <yuvalabeles@mail.tau.ac.il>
2021-12-18 18:21:09 +0200

    we are the bomb

--- FibonacciHeap.java
+++ FibonacciHeap.java
@@ -5,16 +5,28 @@
  */
 public class FibonacciHeap
 {
-
+	private HeapNode min;
+	private HeapNode first;
+	private int size;
+	
+	public FibonacciHeap() {
+		this.min = null;
+		this.first = null;
+		this.size = 0;
+	}
+	
    /**
     * public boolean isEmpty()
     *
     * Returns true if and only if the heap is empty.
     *   
     */
-    public boolean isEmpty()
+    public boolean isEmpty() // Returns true if the size of heap equals 0 --> O(1)
     {
-    	return false; // should be replaced by student code
+    	if (this.size == 0) {
+    		return true;
+    		}
+    	return false; 
     }
 		
    /**
@@ -25,9 +37,25 @@ public class FibonacciHeap
     * 
     * Returns the newly created node.
     */
-    public HeapNode insert(int key)
-    {    
-    	return new HeapNode(key); // should be replaced by student code
+    public HeapNode insert(int key)  // Returns the node we inserted, inserts node with given key as first node, updates all relevant fields --> O(1)
+    {   
+    	HeapNode insertedNode = new HeapNode(key);
+    	if (this.isEmpty()) { // tree is empty, intialize min and first
+    		this.min = insertedNode;
+    		this.first = insertedNode;
+    	}
+    	else {
+    		insertedNode.setNext(this.first);
+    		insertedNode.setPrev(this.first.prev);
+    		this.first.setPrev(insertedNode);
+    		insertedNode.prev.setNext(insertedNode);
+    		this.first = insertedNode;
+    		if (insertedNode.key < this.min.key) { // updates minimal node if true
+    			this.min = insertedNode;
+    		}
+    	}
+    	updateSize(1);
+    	return insertedNode; 
     }
 
    /**
@@ -38,6 +66,8 @@ public class FibonacciHeap
     */
     public void deleteMin()
     {
+    	// dont forget to update size
+    	// dont forget to include edge case where tree is empty and when tree has only one node
      	return; // should be replaced by student code
      	
     }
@@ -48,9 +78,9 @@ public class FibonacciHeap
     * Returns the node of the heap whose key is minimal, or null if the heap is empty.
     *
     */
-    public HeapNode findMin()
+    public HeapNode findMin() // Returns heap's minimal node --> O(1)
     {
-    	return new HeapNode(678);// should be replaced by student code
+    	return this.min;
     } 
     
    /**
@@ -61,6 +91,7 @@ public class FibonacciHeap
     */
     public void meld (FibonacciHeap heap2)
     {
+    	// dont forget to update size
     	  return; // should be replaced by student code   		
     }
 
@@ -72,9 +103,13 @@ public class FibonacciHeap
     */
     public int size()
     {
-    	return -123; // should be replaced by student code
+    	return this.size; // should be replaced by student code
     }
     	
+    private void updateSize(int i) { // Updates size of heap according to change in nodes --> O(1)
+    	this.size += i;
+    }
+    
     /**
     * public int[] countersRep()
     *
@@ -95,11 +130,22 @@ public class FibonacciHeap
 	* It is assumed that x indeed belongs to the heap.
     *
     */
-    public void delete(HeapNode x) 
+    public void delete(HeapNode x) // Deletes the given node --> O(logn)
     {    
-    	return; // should be replaced by student code
+    	if (x == this.min) { // x is already the minimal node
+    		deleteMin();
+    	}
+    	if (this.size == 1) { // only one node in heap
+    		this.min = null;
+    		this.first = null; 
+    	}
+    	else { 
+    		decreaseKey(x,- x.key - Integer.MIN_VALUE); // waiting to get fixed when we know if min can be inf
+        	deleteMin();
+    	}
+    	updateSize(-1);
     }
-
+    
    /**
     * public void decreaseKey(HeapNode x, int delta)
     *
@@ -122,7 +168,8 @@ public class FibonacciHeap
     */
     public int potential() 
     {    
-    	return -234; // should be replaced by student code
+    	
+    	return 3;
     }
 
    /**
@@ -160,6 +207,7 @@ public class FibonacciHeap
     */
     public static int[] kMin(FibonacciHeap H, int k)
     {    
+    	// dont forget to update size if needed
         int[] arr = new int[100];
         return arr; // should be replaced by student code
     }
@@ -174,13 +222,76 @@ public class FibonacciHeap
     public static class HeapNode{
 
     	public int key;
+    	private int rank;
+    	private int mark;  // 0 = unmarked , 1 = marked
+    	private HeapNode child;
+    	private HeapNode next;
+    	private HeapNode prev;
+    	private HeapNode parent; 
+    	
 
     	public HeapNode(int key) {
     		this.key = key;
+    		this.rank = 0;
+    		this.mark = 0;
+    		this.child = null;
+    		this.next = this; //make sure thats correct
+    		this.prev = this;
+    		this.parent = null; 
     	}
 
-    	public int getKey() {
+    	public int getKey() { // Returns node's key --> O(1)
     		return this.key;
+    	}
+    	
+    	public int getRank() { // Returns node's rank --> O(1)
+    		return this.rank;
+    	}
+    	
+    	public void setRank(int k) { // Sets node's rank --> O(1)
+    		this.rank = k;
+    	}
+    	
+    	public int getMark() { // Return node's mark --> O(1)
+    		return this.mark;
+    	}
+    	
+    	public void setMark(int k) { // Sets node's mark, gets only 0 or 1 depending on whether child was cut or not --> O(1)
+    		if ((k==0) || (k==1)) {
+    			this.mark = k;
+    		}
+    	}
+    	
+    	public HeapNode getChild() { // Returns node's child --> O(1)
+    		return this.child;
+    	}
+    	
+    	public void setChild(HeapNode child) { // Sets node's child --> O(1)
+    		this.child = child;
+    	}
+    	
+    	public HeapNode getNext() { // Returns node's right neighbor --> O(1)
+    		return this.next;
+    	}
+    	
+    	public void setNext(HeapNode next) { // Sets node's right neighbor --> O(1)
+    		this.next = next;
+    	}
+    	
+    	public HeapNode getPrev() { // Returns node's left neighbor --> O(1)
+    		return this.prev;
+    	}
+    	
+    	public void setPrev(HeapNode prev) { // Sets node's left neighbor --> O(1)
+    		this.prev = prev;
+    	}
+    	
+    	public HeapNode getParent() { // Returns node's parent --> O(1)
+    		return this.parent;
+    	}
+    	
+    	public void setParent(HeapNode parent) { // Sets node's parent --> O(1)
+    		this.parent = parent;
     	}
     }
 }
